88c2cb34937d05d286b39594ddd82253ea5d1c05
unknown <msulman147@gmail.com>
2024-08-05 15:22:33 +0500

    Created And Up Search Mcicroservice with Mongodb and set dependency of both  service

---
+++ src/SearchServices/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchServices;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDB", MongoClientSettings
+    .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var Items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine("<====================>" + Items.Count + "return from the auctions services<====================>");
+
+        if (Items.Count > 0) await DB.SaveAsync(Items);
+
+    }
+}
